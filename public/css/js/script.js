3beed16b485204f2ad3147c5fe741a075fd8846a
Lady Malig <ladymalig@Ladys-MacBook-Pro.local>
2024-04-12 12:34:07 -0400

    Organize project files into public directory

---
+++ public/css/js/script.js
@@ -0,0 +1,10 @@
+function toggleSection(sectionId) {
+    const sections = document.querySelectorAll('section');
+    sections.forEach(section => {
+        if (section.id === sectionId) {
+            section.style.display = 'block';
+        } else {
+            section.style.display = 'none';
+        }
+    });
+}
